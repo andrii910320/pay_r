1a457fc051133854ecc4307abaae511faadeec13
andrii910320 <andrii.ryzhuk98@gmail.com>
2023-10-23 12:50:40 +0300

    create login

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,38 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import s from "./page.module.scss";
+import iconEmail from "./assets/img/Component 17.png";
+import iconPassword from "./assets/img/Component 18.png";
+import google from "./assets/img/google.png";
 
-export default function Home() {
-  return <>hello</>;
+export default function Login() {
+  return (
+    <div className={s.loginForm}>
+      <div className={s.welcomeBack}>Welcome Back 👋🏼</div>
+      <div className={s.inputArea}>
+        <div className={s.container}>
+          <input className={s.input} placeholder="Email" type="text" />
+          <Image src={iconEmail} className={s.icon} alt="Email" />
+        </div>
+        <div className={s.container}>
+          <input className={s.input} placeholder="Password" type="text" />
+          <Image src={iconPassword} className={s.icon} alt="Password" />
+        </div>
+        <div className={s.forgot}>Forgot your password?</div>
+      </div>
+      <div className={s.areaButton}>
+        <button className={s.login}>Login</button>
+        <div className={s.element}>or</div>
+        <button className={s.signIn}>
+          <Image className={s.iconGoogle} src={google} alt="" /> Sign in with
+          Google
+        </button>
+      </div>
+      <div className={s.register}>
+        Don't have an account?
+        <span>
+          <b> Register</b>
+        </span>
+      </div>
+    </div>
+  );
 }
